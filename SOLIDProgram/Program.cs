eb3d6543758e034faa3d25a1d860e1cccf12c12c
Muhammad K Khan <mk_muhammad.khan@hotmail.com>
2017-01-04 22:12:04 -0600

    Update Program.cs

--- SOLIDProgram/Program.cs
+++ SOLIDProgram/Program.cs
@@ -13,22 +13,5 @@ namespace SOLIDProgram
             SingleResponsibility sr = new SingleResponsibility();
             sr.demo();
         }
-
-        /*
-            public class B {}
-            public class A{
-                private B _b;
-                public B B {
-            get {
-            if(_b == null){
-                    // iniate b
-                }  else{
-                return _b
-                }      
-        }  
-                 
-                
-            }
-         */
     }
 }
